670a54e0ae637ee675c69c0fa14da793bac20b45
lekchu <151711811+lekchu@users.noreply.github.com>
2025-07-12 00:17:16 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import base64
 import joblib
@@ -65,15 +66,14 @@ if st.session_state.page == "intro":
         submitted = st.form_submit_button("Start Questionnaire")
 
         if submitted:
-    st.session_state.user_data = {
-        "Name": name,
-        "Age": age,
-        "Pregnant": pregnant,
-        "RecentBirth": recent_birth,
-        "FamilySupport": family_support
-    }
-    st.session_state.page = "questionnaire"
-    st.experimental_rerun()
+            st.session_state.user_data = {
+                "Name": name,
+                "Age": age,
+                "Pregnant": pregnant,
+                "RecentBirth": recent_birth,
+                "FamilySupport": family_support
+            }
+            st.session_state.page = "questionnaire"
+            st.experimental_rerun()
 
-            
-            go_to("questionnaire")
+        
