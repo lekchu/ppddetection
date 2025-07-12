fdcb855ea7c74e7bbd132ad0f41cfad3aca67b2b
lekchu <151711811+lekchu@users.noreply.github.com>
2025-07-12 16:30:54 +0530

    Delete app.py

--- app.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Postpartum Depression Predictor", layout="wide")
-st.markdown("<h2 style='text-align:center;'>Use the sidebar 👉 to begin the assessment</h2>", unsafe_allow_html=True)
